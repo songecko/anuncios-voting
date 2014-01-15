5f6da29acaf02a108187df753538bd85802ec2da
Gecko <Gecko@Gecko-PC>
2014-01-15 03:28:57 -0300

    anuncio show

--- src/Anuncios/AnuncioBundle/Resources/assets/js/main.js
+++ src/Anuncios/AnuncioBundle/Resources/assets/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function()
+{
+	$(".anuncioShow .picture").hover(function()
+	{
+		$(this).children('.overlay, .overlayContent').show();
+	}, function()
+	{
+		$(this).children('.overlay, .overlayContent').hide();
+	});
+});
