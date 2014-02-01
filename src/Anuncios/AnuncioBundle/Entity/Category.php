f836bb42a479d2dedfb536b6a22848c8eb232f8c
Gecko <Gecko@Gecko-PC>
2014-02-01 03:50:44 -0300

    test slugify

--- src/Anuncios/AnuncioBundle/Entity/Category.php
+++ src/Anuncios/AnuncioBundle/Entity/Category.php
@@ -146,27 +146,29 @@ class Category implements ImageInterface
     
 	public function slugify($text)
   	{
-
-	  // replace non letter or digits by -
-	  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-	
-	  // trim
-	  $text = trim($text, '-');
-	
-	  // transliterate
-	  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-	
-	  // lowercase
-	  $text = strtolower($text);
-	
-	  // remove unwanted characters
-	  $text = preg_replace('~[^-\w]+~', '', $text);
-	
-	  if (empty($text))
-	  {
-	    return 'n-a';
-	  }
-	
-	  return $text;
+  		// replace non letter or digits by -
+  		$text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+  		
+  		// trim
+  		$text = trim($text, '-');
+  		
+  		// transliterate
+  		if (function_exists('iconv'))
+  		{
+  			$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  		}
+  		
+  		// lowercase
+  		$text = strtolower($text);
+  		
+  		// remove unwanted characters
+  		$text = preg_replace('#[^-\w]+#', '', $text);
+  		
+  		if (empty($text))
+  		{
+  			return 'n-a';
+  		}
+  		
+  		return $text;
   	}
 }
