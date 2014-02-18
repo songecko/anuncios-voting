a12c8ae039a8b77f1bc704d82353f25cf3937eff
Gecko <Gecko@Gecko-PC>
2014-02-18 00:57:34 -0300

    portada 2 - show finalistas

--- src/Anuncios/AnuncioBundle/Controller/Frontend/PortadaController.php
+++ src/Anuncios/AnuncioBundle/Controller/Frontend/PortadaController.php
@@ -9,6 +9,42 @@ class PortadaController extends Controller
 {	
 	public function comingSoonAction(Request $request)
 	{
+		$lastCampaignClosed = $this->getDoctrine()->
+			getRepository('AnunciosAnuncioBundle:Campaign')->getLastActiveCampaignClosed();
+		
+		if($lastCampaignClosed)
+		{
+			$categoryfinalists = array();
+			if($lastCampaignClosed->getShowFinalist() == true)
+			{
+				$categories = $this->getDoctrine()
+					->getRepository('AnunciosAnuncioBundle:Category')
+					->getCategoriesWithoutAnual();
+				
+				foreach($categories as $category)
+				{
+					$finalistsJurado = $this->getDoctrine()
+					->getRepository('AnunciosAnuncioBundle:Anuncio')
+					->getAnunciosVoteByJurado($lastCampaignClosed, $category);
+									
+					$finalistsUsuario = $this->getDoctrine()
+					->getRepository('AnunciosAnuncioBundle:Anuncio')
+					->getAnunciosVoteByUsuario($lastCampaignClosed, $category);
+					
+					$categoryfinalists[] = array(
+						'category' => $category,
+						'finalistJurado' => $finalistsJurado,
+						'finalistUsuario' => $finalistsUsuario
+					);
+				}
+			}
+			
+			return $this->render('AnunciosAnuncioBundle:Frontend/Portada:comingSoonNextMonth.html.twig', array(
+					'lastCampaignClosed' => $lastCampaignClosed,
+					'categoryFinalists' => $categoryfinalists
+			));
+		}
+		
     	return $this->render('AnunciosAnuncioBundle:Frontend/Portada:comingSoon.html.twig', array());
 	}
 }
